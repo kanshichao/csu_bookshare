59b1fb3b2487b39da5ace11273c181fb351f9330
kanshichao <kanshichao10281078@126.com>
2025-04-01 22:34:34 +0800

    Create index.js

---
+++ components/loading/index.js
@@ -0,0 +1,13 @@
+// components/loading/index.js
+Component({
+  properties: {
+    color: {
+      type: String,
+      value: '#2d8cf0'
+    },
+    size: {
+      type: String,
+      value: '40rpx'
+    }
+  }
+});
